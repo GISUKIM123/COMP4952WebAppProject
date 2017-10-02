90d1f62bd435617dc05722215a7054fa966fbbdc
Gisu Kim <sksmszhdzk@gmail.com>
2017-10-02 09:14:22 -0700

    framework upload with mongoDB embeded

---
+++ TechproMiniMart/TechproMiniMart/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes; 
+
+
+namespace TechproMiniMart.Models
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId id { get; set; }
+
+        [BsonElement()]
+        public string fname { get; set; }
+        [BsonElement()]
+        public string lname { get; set; }
+        [BsonElement()]
+        public string phonenumber { get; set; }
+
+    }
+}
---
+++ TechproMiniMart/TechproMiniMart/Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Configuration;
+
+
+namespace TechproMiniMart.Models
+{
+    public class UserModel
+    {
+        private MongoClient mongoClient;
+        private IMongoCollection<User> userCollection;
+
+        public UserModel()
+        {
+            mongoClient = new MongoClient(ConfigurationManager.AppSettings["mongoDBHost"]);
+            var db = mongoClient.GetDatabase(ConfigurationManager.AppSettings["mongoDBName"]);
+
+            userCollection = db.GetCollection<User>("usercollection");
+
+        }
+
+        public List<User> findAll()
+        {
+            return userCollection.AsQueryable<User>().ToList();
+        }
+    }
+}
